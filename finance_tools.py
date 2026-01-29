b61edda7b340230bd291f6f644650444b21c14cf
Tanmay Kakade <tanmaykakade5@gmail.com>
2026-01-28 20:41:57 -0600

    Added finance tool code

---
+++ finance_tools.py
@@ -0,0 +1,26 @@
+from netsuite_client import NetSuiteClient
+
+
+def get_overdue_invoices(days: int = 30) -> dict:
+    """
+    Returns customer invoices that are overdue within the last N days.
+    """
+    client = NetSuiteClient()
+
+    query = f"""
+    SELECT
+        t.id,
+        t.tranid,
+        t.entity,
+        t.trandate,
+        t.duedate,
+        t.amountremaining
+    FROM transaction t
+    WHERE t.type = 'CustInvc'
+      AND t.amountremaining > 0
+      AND t.duedate < CURRENT_DATE
+      AND t.duedate >= (CURRENT_DATE - {int(days)})
+    ORDER BY t.duedate ASC
+    """
+
+    return client.suiteql(query)
